5854851d3d9d2458c8d495a06a104687dabfbbad
Joshua Rully <joshrully@gmail.com>
2021-02-16 01:10:54 +0000

    added bodyParser functionality to newUrl form

--- expressServer.js
+++ expressServer.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//GETs below!
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -29,6 +34,13 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urlsShow", templateVars);
+});
+
+//POSTs below!
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
