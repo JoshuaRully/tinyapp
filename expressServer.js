124404d1dc5c579b90e00369172f8f558a9a4590
Joshua Rully <joshrully@gmail.com>
2021-02-16 04:34:38 +0000

    refactored

--- expressServer.js
+++ expressServer.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const app = express();
-const PORT = 8080;
-
 const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
+
+const PORT = 8080;
 
 const genRandomString = () => {
   let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
+  const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = char.length;
   for (let i = 0; i < 6; i++) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += char.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
-
-app.set('view engine', 'ejs');
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -42,27 +44,29 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urlsShow", templateVars);
 });
 
 app.get('/u/:shortURL', (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
-    const longURL = urlDatabase[req.params.shortURL]
+    const longURL = urlDatabase[req.params.shortURL];
     res.redirect(longURL);
   } else {
+    // TODO: find better status code
     res.redirect('https://http.cat/404');
   }
 });
   
-
 // POSTs below!
 
 app.post("/urls", (req, res) => {
   const shortURL = genRandomString();
-  const longURL = req.body.longURL;
+  const { longURL } = req.body;
   urlDatabase[shortURL] = longURL;
-  console.log(urlDatabase);
   res.redirect('/urls/' + String(shortURL));
 });
 
@@ -75,5 +79,5 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 // The Gnomes are listening... :o
 
 app.listen(PORT, () => {
-  console.log(`Example app is listening on port ${PORT}!`)
+  console.log(`Example app is listening on port ${PORT}!`);
 });
